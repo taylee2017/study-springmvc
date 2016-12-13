2b14c5f2661724cb35389337450129da2b3ee8df
apple <taylee.com.cn@gmail.com>
2016-12-14 00:27:31 +0800

    提交内容：配置返回数据为json格式(@ResponseBody方式) 提交人：taylee

---
+++ src/main/java/cn/com/taylee/bean/StudyBean.java
@@ -0,0 +1,55 @@
+package cn.com.taylee.bean;
+
+import java.util.Date;
+
+/**
+ * Created by apple on 16-12-13.
+ */
+public class StudyBean {
+
+    private String id;
+    private String name;
+    private String title;
+    private String content;
+    private Date time;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+}
--- src/main/java/cn/com/taylee/controller/IndexController.java
+++ src/main/java/cn/com/taylee/controller/IndexController.java
@@ -1,10 +1,14 @@
 package cn.com.taylee.controller;
 
+import cn.com.taylee.bean.StudyBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
 
 /**
  * Created by apple on 16-12-7.
@@ -17,6 +21,37 @@ public class IndexController {
     public String mainPage(@PathVariable("name") String name, Model model){
         model.addAttribute("test", name);
         return "index";
+    }
+
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    @ResponseBody
+    public StudyBean getInfo(){
+        StudyBean studyBean = new StudyBean();
+        studyBean.setId("1");
+        studyBean.setName("aa");
+        studyBean.setTitle("bb");
+        studyBean.setContent("cc");
+        studyBean.setTime(new Date());
+        return studyBean;
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String,Object> listStudy(){
+        List<StudyBean> listRes = new ArrayList<StudyBean>();
+        for(int i = 0;i < 5;i ++){
+            StudyBean studyBean = new StudyBean();
+            studyBean.setId("id" + i);
+            studyBean.setName("name" + i);
+            studyBean.setTitle("title" + i);
+            studyBean.setContent("content" + i);
+            studyBean.setTime(new Date());
+            listRes.add(studyBean);
+        }
+        Map<String,Object> mapModel = new HashMap<String,Object>();
+        mapModel.put("status","true");
+        mapModel.put("result",listRes);
+        return mapModel;
     }
 
 }
